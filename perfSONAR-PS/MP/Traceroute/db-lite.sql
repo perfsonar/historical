3c4ece444cf99a4aad902a3cb84d1f3655321413
zurawski <zurawski@752d5dfb-d90f-4296-8230-53451a7f88fe>
2007-06-15 13:47:01 +0000

    Minor changes to the Ping MP.
    Addition of the Traceroute MP/MA (from Ben Perry <pianoman@udel.edu> who 
    was having SVN problems that prevented him from uploading himself).  
    This MP/MA works with a file DB (for now, I will be adding XMLDB support 
    in time) and stores to an SQL database.
    
    It uses the Net::Traceroute API and currently only supports a single 
    parameter (num queries per hop).  
    
    -jason
    
    
    
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@229 752d5dfb-d90f-4296-8230-53451a7f88fe

---
+++ perfSONAR-PS/MP/Traceroute/db-lite.sql
@@ -0,0 +1,8 @@
+create table data (
+  id			int unsigned not null,  
+  time			int unsigned not null,
+  value			int unsigned,
+  eventtype		varchar(128) not null,
+  misc			varchar(128), 
+  unique (id, time, value, eventtype, misc)
+);
