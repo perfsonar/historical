7e9c99f70a73b6854d5bdeab4f79168f2b7781a7
maxim <maxim@752d5dfb-d90f-4296-8230-53451a7f88fe>
2008-04-11 20:02:45 +0000

    added libxml2 libs
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@1908 752d5dfb-d90f-4296-8230-53451a7f88fe

--- scripts/make_exec.sh
+++ scripts/make_exec.sh
@@ -1,4 +1,4 @@
-#!/bin/env  sh
+#!/bin/env  bash
 
  
 tolower()
@@ -14,18 +14,35 @@ return $retval
 }
 
 
+XMLLIB=$(xml2-config  --libtool-libs)
+if [ -z $XMLLIB  ]
+then
+echo " libxml2 libraries must be installed on building host "
+exit
+fi  
+
 MODULE=$1
+
+if ( [ ! -e "../lib/perfSONAR_PS/Services/MA/$MODULE.pm" ] || [ !  -e "../lib/perfSONAR_PS/Services/MP/$MODULE.pm" ] )
+then
+echo " Usage: make_exec.sh <service module name  - for example PingER> "
+echo "        for service XXX it checks  module  ../lib/perfSONAR_PS/Services/M[PA]/XXX.pm "
+exit
+fi
+
 EXECNAME=$(tolower $MODULE) 
 echo "Building executable ps-$EXECNAME  for $MODULE MA and MP"
 
-pp -I ../lib/ -M DBD::mysql  -M perfSONAR_PS::Services::LS  \
+COM="pp -I ../lib/ -M DBD::mysql  -M perfSONAR_PS::Services::LS  \
  -M perfSONAR_PS::Services::Echo -M  perfSONAR_PS::Request \
- -M  perfSONAR_PS::RequestHandler  -M perfSONAR_PS::DB::RRD \
+ -M perfSONAR_PS::RequestHandler  -M perfSONAR_PS::DB::RRD \
  -M perfSONAR_PS::DB::File  -M perfSONAR_PS::DB::XMLDB \
  -M perfSONAR_PS::DB::SQL -M perfSONAR_PS::DB::SQL::PingER \
  -M fields -M DateTime::Locale::en  \
  -M perfSONAR_PS::Services::MP::$MODULE  \
  -M perfSONAR_PS::Services::MA::$MODULE \
- -o ps-$EXECNAME  ../perfsonar-daemon.pl
+ -l  $XMLLIB  \
+ -o ps-$EXECNAME  ../perfsonar-daemon.pl"
 
-
+echo -e " Building ... \n  $COM  \n "
+$COM
