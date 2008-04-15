4038808b136d0aecbd97aa49cae741b58af04723
maxim <maxim@752d5dfb-d90f-4296-8230-53451a7f88fe>
2008-04-15 22:40:20 +0000

    added sqlite and xmldb modules to be loaded
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@1914 752d5dfb-d90f-4296-8230-53451a7f88fe

--- scripts/make_exec.sh
+++ scripts/make_exec.sh
@@ -30,7 +30,7 @@ MODULE=$1
 if ( [ ! -e "../lib/perfSONAR_PS/Services/MA/$MODULE.pm" ] || [ !  -e "../lib/perfSONAR_PS/Services/MP/$MODULE.pm" ] )
 then
 echo " Usage: make_exec.sh <service module name  - for example PingER> "
-echo "        for service XXX it checks  module  ../lib/perfSONAR_PS/Services/M[PA]/XXX.pm "
+echo "        for service XXX next modules must exist: ../lib/perfSONAR_PS/Services/M[PA]/XXX.pm "
 exit
 fi
  
@@ -38,7 +38,7 @@ fi
 EXECNAME=$(tolower $MODULE) 
 echo "Building executable ps-$EXECNAME  for $MODULE MA and MP"
  
-COM="pp -I ../lib/ -M DBD::mysql  -M perfSONAR_PS::Services::LS  \
+COM="pp -I ../lib/ -M DBD::mysql -M  Sleepycat::DbXml  -M DBD::SQLite -M perfSONAR_PS::Services::LS  \
  -M perfSONAR_PS::Services::Echo -M  perfSONAR_PS::Request \
  -M perfSONAR_PS::RequestHandler  -M perfSONAR_PS::DB::RRD \
  -M perfSONAR_PS::DB::File  -M perfSONAR_PS::DB::XMLDB \
