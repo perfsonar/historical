acbf632db4653503402fdf5f0f31be22d347b6d4
aaron <aaron@752d5dfb-d90f-4296-8230-53451a7f88fe>
2008-06-06 17:40:19 +0000

    Add support for grabbing line/section information from the HDXc to the TL1 module.
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@1989 752d5dfb-d90f-4296-8230-53451a7f88fe

--- lib/perfSONAR_PS/Utils/TL1/HDXc.pm
+++ lib/perfSONAR_PS/Utils/TL1/HDXc.pm
@@ -8,7 +8,7 @@ use Params::Validate qw(:all);
 use Data::Dumper;
 
 use base 'perfSONAR_PS::Utils::TL1::Base';
-use fields 'CRSSBYNAME', 'ETHSBYAID', 'OCNSBYAID', 'OCNSBYNAME', 'READ_CRS', 'READ_ETH', 'READ_OCN';
+use fields 'CRSSBYNAME', 'ETHSBYAID', 'OCNSBYAID', 'OCNSBYNAME', 'SECTSBYAID', 'LINESBYAID', 'LINESBYNAME', 'READ_CRS', 'READ_ETH', 'READ_OCN', 'READ_SECT', 'READ_LINE';
 
 sub initialize {
     my ($self, @params) = @_;
@@ -76,6 +76,66 @@ sub getGTP {
     return;
 }
 
+sub getSect {
+    my ($self, $aid) = @_;
+    my $do_reload_stats = 0;
+
+    if (not $self->{READ_SECT}) {
+        $do_reload_stats = 1;
+        $self->{READ_SECT} = 1;
+    }
+
+    if ($self->{CACHE_TIME} + $self->{CACHE_DURATION} < time or $do_reload_stats) {
+        $self->readStats();
+    }
+
+    if (not $aid) {
+        return $self->{SECTSBYAID};
+    } else {
+        return $self->{SECTSBYAID}->{$aid};
+    }
+}
+
+sub getLineByName {
+    my ($self, $name) = @_;
+    my $do_reload_stats = 0;
+
+    if (not $self->{READ_LINE}) {
+        $do_reload_stats = 1;
+        $self->{READ_LINE} = 1;
+    }
+
+    if ($self->{CACHE_TIME} + $self->{CACHE_DURATION} < time or $do_reload_stats) {
+        $self->readStats();
+    }
+
+    if (not $name) {
+        return $self->{LINESBYNAME};
+    } else {
+        return $self->{LINESBYNAME}->{$name};
+    }
+}
+
+sub getLine {
+    my ($self, $aid) = @_;
+    my $do_reload_stats = 0;
+
+    if (not $self->{READ_LINE}) {
+        $do_reload_stats = 1;
+        $self->{READ_LINE} = 1;
+    }
+
+    if ($self->{CACHE_TIME} + $self->{CACHE_DURATION} < time or $do_reload_stats) {
+        $self->readStats();
+    }
+
+    if (not $aid) {
+        return $self->{LINESBYAID};
+    } else {
+        return $self->{LINESBYAID}->{$aid};
+    }
+}
+
 sub getCrossconnect {
     my ($self, $name) = @_;
     my $do_reload_stats = 0;
@@ -128,8 +188,73 @@ sub readStats {
         $self->readOCNs();
     }
 
+    if ($self->{READ_SECT}) {
+        $self->readSECTs();
+    }
+
+    if ($self->{READ_LINE}) {
+        $self->readLINEs();
+    }
+
     $self->{CACHE_TIME} = time;
     $self->{TL1AGENT}->disconnect();
+
+    return;
+}
+
+sub readSECTs {
+    my ($self) = @_;
+
+    my %sects = ();
+
+    my @results = $self->send_cmd("RTRV-SECT:::1234;");
+
+    foreach my $line (@results) {
+        if ($line =~ /"([^:]*):[^:]*:[^:]*:([A-Z]*),([A-Z]*)"/) {
+            my ($aid, $name, $pst, $sst);
+
+            $aid = $1;
+            $pst = $2;
+            $sst = $3;
+
+            my %status = ( sst => $sst, pst => $pst );
+
+            $sects{$aid} = \%status;
+        }
+    }
+
+    $self->{SECTSBYAID} = \%sects;
+
+    return;
+}
+
+sub readLINEs {
+    my ($self) = @_;
+
+    my %lines = ();
+    my %lines_name = ();
+
+    my @results = $self->send_cmd("RTRV-LINE:::1234;");
+
+    foreach my $line (@results) {
+        if ($line =~ /"([^:]*):.*:.*LABEL=\\"([^\\]*)\\".*:([A-Z]*)"/) {
+            my ($aid, $name, $pst, $sst);
+
+            $aid = $1;
+            $name = $2;
+            $pst = $3;
+
+            my %status = ( sst => $sst, pst => $pst );
+
+            $lines{$aid} = \%status;
+            if ($name) {
+                $lines_name{$name} = \%status;
+            }
+        }
+    }
+
+    $self->{LINESBYAID} = \%lines;
+    $self->{LINESBYNAME} = \%lines_name;
 
     return;
 }
@@ -140,8 +265,7 @@ sub readOCNs {
     my %ocns = ();
     my %ocns_name = ();
 
-#    foreach my $i (3, 12, 48, 192) {
-    foreach my $i (192) {
+    foreach my $i (3, 12, 48, 192) {
         my @results = $self->send_cmd("RTRV-OC".$i.":::1234;");
 
         foreach my $line (@results) {
