101abd4548606c267ca7d85884db90faad9b64e1
aaron <aaron@752d5dfb-d90f-4296-8230-53451a7f88fe>
2008-06-27 15:42:22 +0000

    Update the TL1 SQL initialization script
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@2048 752d5dfb-d90f-4296-8230-53451a7f88fe

--- scripts/tl1/tl1.sql
+++ scripts/tl1/tl1.sql
@@ -35,13 +35,31 @@ CREATE TABLE ps_tl1_alarms (
   description VARCHAR(32) NOT NULL,
   location VARCHAR(4) NOT NULL,
   supplemental VARCHAR(32) NOT NULL,
-  UNIQUE (interface_id, type, start_time, time_period, direction),
+  UNIQUE (interface_id, condition_type, time),
   FOREIGN KEY (interface_id) references ps_tl1_interfaces(id));
 
 CREATE TRIGGER ps_tl1_alarms_interface_id_fki
   BEFORE INSERT ON ps_tl1_alarms FOR EACH ROW
   BEGIN
     SELECT RAISE(ROLLBACK, 'Insert on ps_tl1_alarms violates foreign key on interface_id')
+    WHERE 
+      (SELECT id FROM ps_tl1_interfaces WHERE id = NEW.interface_id)
+        IS NULL;
+  END;
+
+CREATE TABLE ps_tl1_states (
+  interface_id INTEGER NOT NULL,
+  start_time INTEGER NOT NULL,
+  end_time INTEGER NOT NULL,
+  primary_state VARCHAR(10) NOT NULL,
+  secondary_state VARCHAR(10) NOT NULL,
+  UNIQUE (interface_id, start_time, end_time),
+  FOREIGN KEY (interface_id) references ps_tl1_interfaces(id));
+
+CREATE TRIGGER ps_tl1_states_interface_id_fki
+  BEFORE INSERT ON ps_tl1_states FOR EACH ROW
+  BEGIN
+    SELECT RAISE(ROLLBACK, 'Insert on ps_tl1_states violates foreign key on interface_id')
     WHERE 
       (SELECT id FROM ps_tl1_interfaces WHERE id = NEW.interface_id)
         IS NULL;
