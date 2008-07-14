fe9a401be24ede92a9bd43801179a155642f38d8
maxim <maxim@752d5dfb-d90f-4296-8230-53451a7f88fe>
2008-07-14 23:24:18 +0000

    updated to most current version
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@2094 752d5dfb-d90f-4296-8230-53451a7f88fe

--- modules/perfSONAR_PS-Services-PingER-v0.01/util/pinger_schema.pl
+++ modules/perfSONAR_PS-Services-PingER-v0.01/util/pinger_schema.pl
@@ -1,9 +1,9 @@
-#!/usr/bin/perl -w
+#!/usr/local/bin/perl -w
  use lib qw(../lib);
  use warnings;
  use strict; 
  use File::Path;
- use perfSONAR_PS::SimpleConfig;
+ use Config::Interactive;
  
  
  
@@ -12,7 +12,8 @@
     Log::Log4perl->init("../bin/logger.conf"); 
  };
  use   version;
-     
+
+ use   perfSONAR_PS::DataModels::PingER_Topology  2.0 qw($pingertopo);        
  use   perfSONAR_PS::DataModels::PingER_Model 2.0 qw($message);
  use   perfSONAR_PS::DataModels::APIBuilder   2.0 qw(&buildAPI  $API_ROOT $TOP_DIR $SCHEMA_VERSION $DATATYPES_ROOT $TEST_DIR);
     
@@ -21,15 +22,11 @@
   
    my %CONF_PROMPTS = (   "METADATA_DB_TYPE" => "type of the internal metaData DB ( file| xmldb | sql ) ", 
                           "METADATA_DB_NAME" => " name of the internal   metaData  DB ", 
-			  'DB_USER' =>    '  username to connect to the data SQL DB ',
-                          'DB_PASS' =>    ' password to connect to the data SQL DB ',
 			  'TOP_DIR' =>    '  top directory   where to build API ',
 			  'API_ROOT'=>   ' root package name for the API', 
 			  'TEST_DIR' => ' top directory  where to build tests files ', 
 			  'DATATYPES_ROOT' => ' top directory name where to place versioned datatypes API',  
-			  'DB_DRIVER' =>  '  perl driver name of the  data SQL DB ',
-			  'CONFIGURE_DB' => ' by answering yes you will load yor DB configuration and create all object files ',
-			  'DB_CLASSNAME' => ' class prefix for database packages',
+			 
 		      );
 
 		# Read in configuration information
@@ -37,9 +34,11 @@
 #
 #   pinger configuration part is here
 # 
-  my $pingerMA_conf = perfSONAR_PS::SimpleConfig->new( -FILE => 'pingerMA_model.conf', 
-                                                     -PROMPTS => \%CONF_PROMPTS, 
-						     -DIALOG => '1');
+  my $pingerMA_conf =  Config::Interactive->new( { file=> 'pingerMA_model.conf', 
+                                                   prompts   => \%CONF_PROMPTS, 
+						   dialog => '1'
+						 }
+					       );
   $pingerMA_conf->parse();  
   $pingerMA_conf->store;
   my $configh =$pingerMA_conf->getNormalizedData;
@@ -58,31 +57,7 @@
   $TOP_DIR = $configh->{"TOP_DIR"}; 
   #   $TOP_DIR = "/tmp/API/"; 
   $TEST_DIR = $configh->{"TEST_DIR"};
-  if($configh->{"CONFIGURE_DB"} && $configh->{"CONFIGURE_DB"} eq 'yes') {  
-      require   Rose::DB::Object::Loader;
-    
-      my $dbloader =  undef;
-      eval {
-         $dbloader =   Rose::DB::Object::Loader->new(
-                    
-		    db_dsn =>      "DBI:" . $configh->{"SQL_DB_DRIVER"} .":dbname=".$configh->{"SQL_DB_NAME"} ,
-                    db_username =>   $configh->{SQL_DB_USER},
-                    db_password =>   $configh->{SQL_DB_PASS},
- 	            class_prefix =>  $configh->{DB_CLASSNAME},
-		    include_tables => '^(metaData|data\_\d+|host)$',
-		    module_dir =>    $configh->{TOP_DIR},
-		    db_options => { AutoCommit =>1, ChopBlanks => 1, 
-                            RaiseError => 0, PrintError => 1},  
-                );
-      };
-      if($@) {
-         $logger->fatal("Failed to load DB : " . $@);
-         exit(1);
-      }
-    
-      my @dbhooks =  $dbloader->make_modules(); 
-  } 
-   
+  
   mkpath ([ "$TOP_DIR"  ], 1, 0755) ; 
   $TOP_DIR .=  $API_ROOT;  
   eval {
@@ -90,4 +65,10 @@
   };
   if($@) {
        $logger->fatal(" Building API failed " . $@);
+  }
+  eval {
+      buildAPI('topology', $pingertopo,  '', '', '' )
+  };
+   if($@) {
+      $logger->fatal(" Building Topology API failed " . $@);
   }
