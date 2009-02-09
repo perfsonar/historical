e8101b26c5dc00feb98f05214d3b7417ba437c61
aaron <aaron@752d5dfb-d90f-4296-8230-53451a7f88fe>
2009-02-09 18:52:11 +0000

    Update the dependencies
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@2456 752d5dfb-d90f-4296-8230-53451a7f88fe

--- modules/perfSONAR_PS-Status/scripts/install_dependencies.sh
+++ modules/perfSONAR_PS-Status/scripts/install_dependencies.sh
@@ -5,12 +5,21 @@ if [[ $EUID -ne 0 ]]; then
     MAKEROOT="sudo "
 fi
 
-$MAKEROOT cpan DBI
+$MAKEROOT cpan Carp
+$MAKEROOT cpan Config::General
+$MAKEROOT cpan Cwd
 $MAKEROOT cpan DBD::SQLite
+$MAKEROOT cpan DBI
 $MAKEROOT cpan Data::Dumper
+$MAKEROOT cpan Data::UUID
 $MAKEROOT cpan Digest::MD5
 $MAKEROOT cpan English
+$MAKEROOT cpan Error
 $MAKEROOT cpan Exporter
+$MAKEROOT cpan Fcntl
+$MAKEROOT cpan File::Basename
+$MAKEROOT cpan Getopt::Long
+$MAKEROOT cpan HTTP::Daemon
 $MAKEROOT cpan IO::File
 $MAKEROOT cpan LWP::Simple
 $MAKEROOT cpan LWP::UserAgent
@@ -18,10 +27,9 @@ $MAKEROOT cpan Log::Log4perl
 $MAKEROOT cpan Module::Load
 $MAKEROOT cpan Net::Ping
 $MAKEROOT cpan Net::SNMP
+$MAKEROOT cpan Net::Telnet
+$MAKEROOT cpan POSIX
 $MAKEROOT cpan Params::Validate
 $MAKEROOT cpan Time::HiRes
 $MAKEROOT cpan XML::LibXML
 $MAKEROOT cpan base
-$MAKEROOT cpan fields
-$MAKEROOT cpan strict
-$MAKEROOT cpan warnings
