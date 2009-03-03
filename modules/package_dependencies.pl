b4613bec8fb342df93d58087cd229aaea8b9882e
aaron <aaron@752d5dfb-d90f-4296-8230-53451a7f88fe>
2009-03-03 21:51:34 +0000

    Parallel commit to the changes in the branch.
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@2493 752d5dfb-d90f-4296-8230-53451a7f88fe

--- modules/package_dependencies.pl
+++ modules/package_dependencies.pl
@@ -100,9 +100,12 @@ while ($files_left) {
         while(<DEPENDS>) {
             my $module;
 
-            if (/use base '([a-z_A-Z0-9]*(::[a-z_A-Z0-9]*)*)'/) {
+            if (/use base ['"]([a-z_A-Z0-9]*(::[a-z_A-Z0-9]*)*)['"]/) {
                 $module = $1;
 				$dependencies{"base"} = 1;
+            } elsif (/use aliased ['"]([a-z_A-Z0-9]*(::[a-z_A-Z0-9]*)*)['"]/) {
+                $module = $1;
+				$dependencies{"aliased"} = 1;
             } elsif (/use ([a-z_A-Z0-9]*(::[a-z_A-Z0-9]*)*)/) {
                 $module = $1;
             } elsif (/load ([a-z_A-Z0-9]*(::[a-z_A-Z0-9]*)*)/) {
