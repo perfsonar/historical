d575f7de6911f0673f935c124c7c904353dff7ac
zurawski <zurawski@752d5dfb-d90f-4296-8230-53451a7f88fe>
2009-07-10 11:16:21 +0000

    Src install script modifications
    -jason
    
    
    
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/branches/RELEASE_3.1@2912 752d5dfb-d90f-4296-8230-53451a7f88fe

--- perfSONAR_PS-perfAdmin/scripts/prepare_environment.sh
+++ perfSONAR_PS-perfAdmin/scripts/prepare_environment.sh
@@ -6,6 +6,8 @@ then
     MAKEROOT="sudo "
 fi
 
+DIRECTORY=`dirname "$0"`
+
 $MAKEROOT /usr/sbin/groupadd perfsonar 2> /dev/null || :
 $MAKEROOT /usr/sbin/useradd -g perfsonar -s /sbin/nologin -c "perfSONAR User" -d /tmp perfsonar 2> /dev/null || :
 
@@ -13,5 +15,24 @@ $MAKEROOT mkdir -p /var/log/perfsonar
 $MAKEROOT chown perfsonar:perfsonar /var/log/perfsonar
 
 $MAKEROOT mkdir -p /var/lib/perfsonar/perfAdmin/cache
-
 $MAKEROOT chown -R perfsonar:perfsonar /var/lib/perfsonar/perfAdmin
+
+$MAKEROOT mv $DIRECTORY/perfAdmin.cron /etc/cron.d
+$MAKEROOT chown root:root /etc/cron.d/perfAdmin.cron
+$MAKEROOT chmod 600 /etc/cron.d/perfAdmin.cron
+
+$MAKEROOT mv $DIRECTORY/perfAdmin.conf /etc/httpd/conf.d
+$MAKEROOT chown root:root /etc/httpd/conf.d/perfAdmin.conf
+$MAKEROOT chmod 644 /etc/httpd/conf.d/perfAdmin.conf
+
+$MAKEROOT mkdir -p /opt/perfsonar_ps/perfAdmin
+$MAKEROOT mv $DIRECTORY/../bin /opt/perfsonar_ps/perfAdmin
+$MAKEROOT mv $DIRECTORY/../cgi-bin /opt/perfsonar_ps/perfAdmin
+$MAKEROOT mv $DIRECTORY/../doc /opt/perfsonar_ps/perfAdmin
+$MAKEROOT mv $DIRECTORY/../etc /opt/perfsonar_ps/perfAdmin
+$MAKEROOT mv $DIRECTORY/../lib /opt/perfsonar_ps/perfAdmin
+$MAKEROOT chown -R perfsonar:perfsonar /opt/perfsonar_ps/perfAdmin
+$MAKEROOT chown -R apache:apache /opt/perfsonar_ps/perfAdmin/etc
+
+$MAKEROOT /etc/init.d/crond restart
+$MAKEROOT /etc/init.d/httpd restart
