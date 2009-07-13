5290996924a56539cbc5118d02889eac2aa2d2f5
zurawski <zurawski@752d5dfb-d90f-4296-8230-53451a7f88fe>
2009-07-13 14:37:05 +0000

    seperate script that handles client vs server environment
    -jason
    
    
    
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/branches/RELEASE_3.1@2937 752d5dfb-d90f-4296-8230-53451a7f88fe

---
+++ perfSONAR_PS-perfSONARBUOY/scripts/prepare_environment_client.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MAKEROOT=""
+if [[ $EUID -ne 0 ]];
+then
+    MAKEROOT="sudo "
+fi
+
+$MAKEROOT /usr/sbin/groupadd perfsonar 2> /dev/null || :
+$MAKEROOT /usr/sbin/useradd -g perfsonar -s /sbin/nologin -c "perfSONAR User" -d /tmp perfsonar 2> /dev/null || :
+
+$MAKEROOT mkdir -p /var/log/perfsonar
+$MAKEROOT chown perfsonar:perfsonar /var/log/perfsonar
+
+
+$MAKEROOT mkdir -p /var/lib/perfsonar/snmp_ma
+if [ ! -f /var/lib/perfsonar/snmp_ma/store.xml ];
+then
+    $MAKEROOT `dirname $0`/../scripts/makeStore.pl /var/lib/perfsonar/snmp_ma 1
+fi
+
+$MAKEROOT chown -R perfsonar:perfsonar /var/lib/perfsonar/snmp_ma
