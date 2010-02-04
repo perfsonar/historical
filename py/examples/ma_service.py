dc1a1363afe589215e38f23c5fe5889f03ef8a9c
mgoode <mgoode@752d5dfb-d90f-4296-8230-53451a7f88fe>
2010-02-04 22:49:49 +0000

    Updating server example showing how the messager reader/builder objects would be used in the dispatch framework.
    git-svn-id: http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk@3860 752d5dfb-d90f-4296-8230-53451a7f88fe

--- py/examples/ma_service.py
+++ py/examples/ma_service.py
@@ -1,6 +1,7 @@
 from perfsonar.server import psService, serveWithCherryPy
-from lxml import etree
+from perfsonar.message import psMessageBuilder, psMessageReader
 
+from time import time
 import logging
 log = logging.getLogger(__name__)
 
@@ -33,9 +34,59 @@ class MeasurementArchiveService(psService):
     # lxml.etree objects.  Soaplib does an "import as" maneuver 
     # presumably since they used to use the ElementTree lib.
     def metadataKeyRequest(self, message):
-        log.debug('Fetching...')
-        # do something
-        return message
+        log.debug('Fetching metadata...')
+        messageIn = psMessageReader(message)
+        # Read data from the request.
+        requestId = messageIn.getMessageId()
+        dataBlockId = messageIn.getDataBlockIds()[0]
+        data = messageIn.getData(dataBlockId)
+        metadataRef = data.attribs['metadataIdRef']
+        md = messageIn.getMetadata(metadataRef)
+        subjectNS = md.subject['ns']
+        subjectId = md.subject['id']
+        eventType = md.eventType
+        # Log for debug
+        log.debug('*** request id: %s' % requestId)
+        log.debug('*** data id: %s' % dataBlockId)
+        log.debug('*** metadata ref/id: %s' % metadataRef)
+        log.debug('*** subject ns: %s' % subjectNS)
+        log.debug('*** subject id: %s' % subjectId)
+        log.debug('*** event type: %s' % eventType)
+        
+        # Create the response message component.
+        messageOut = psMessageBuilder('message.%s' % time(), 'MetadataKeyResponse',
+                                        messageIdRef=requestId)
+                                        
+        # Do some looping retrieval computations and add the appropriate 
+        # sections to the response.  Using faked up data here from some
+        # other SNMP MA for an example.
+        for i in range(0, 1):
+            metadataId = 'metadata.%s' % time()
+            interfaceData = {
+            'ifAddress type="ipv4"':'64.57.28.58',
+            'hostName':'rtr.atla.net.internet2.edu',
+            'ifName':'xe-2/0/0.0',
+            'direction':'in',
+            'capacity':'10000000000',
+            'description':'BACKBONE: ATLA-WASH 10GE | I2-ATLA-WASH-10GE-05133'
+            }
+            metaParams = {'supportedEventType':['http://ggf.org/ns/nmwg/tools/snmp/2.0',
+            'http://ggf.org/ns/nmwg/characteristic/utilization/2.0']}
+            messageOut.addMetadataBlock(metadataId,
+                        metadataIdRef='rtr.atla.net.internet2.edu--xe-2/0/0.0-in',
+                        subject='64.57.28.243',
+                        subjectType='interface',
+                        subjectData=interfaceData,
+                        eventType='http://ggf.org/ns/nmwg/characteristic/utilization/2.0',
+                        params=metaParams,
+                        paramid='1')
+            dataBlockId = 'data.%s' % time()
+            messageOut.addDataBlock(dataBlockId, metadataIdRef=metadataId)
+            
+            keyParams = {'maKey': 'a593c5016d0778e59d76c6ba42fffc3a'}
+            messageOut.addKeyToDataBlock(dataBlockId, paramid='params.0',params=keyParams)
+                                        
+        return messageOut.message
         
     def measurementStoreRequest(self,message):
         log.debug('Storing...')
@@ -45,7 +96,8 @@ class MeasurementArchiveService(psService):
 if __name__ == '__main__':
     # Change the logging level to DEBUG to see the messages
     # and such.
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(levelname)s: %(name)s: %(funcName)s : %(message)s")
     service = MeasurementArchiveService()
     log.debug("starting server")
     # Ctrl-C will kill the server.
